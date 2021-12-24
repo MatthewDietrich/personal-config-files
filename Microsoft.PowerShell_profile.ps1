aabd8b69f23a595288ee4ebe205158294b2af512
Matt Dietrich <mdietrich@ecisolutions.com>
2021-12-23 18:09:46 -0600

    Add PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh --init --shell pwsh --config "C:\Users\mdietrich\atomicBit_mine.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+Set-Location C:\Users\mdietrich
+
+Set-Alias -Name vi -Value vim
